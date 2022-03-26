62b1b31e43801cd2a58ab8510f6beafd5fe779a8
Mark Gardner <markgard@gmail.com>
2022-03-25 21:23:15 -0500

    Out of Scope HAL

--- Marlin/src/HAL/LPC1768/MarlinSPI.h
+++ Marlin/src/HAL/LPC1768/MarlinSPI.h
@@ -21,7 +21,25 @@
  */
 #pragma once
 
-extern IWDG_HandleTypeDef hiwdg;
+#include <SPI.h>
 
-void watchdog_init();
-void HAL_watchdog_refresh();
+/**
+ * Marlin currently requires 3 SPI classes:
+ *
+ * SPIClass:
+ *  This class is normally provided by frameworks and has a semi-default interface.
+ *  This is needed because some libraries reference it globally.
+ *
+ * SPISettings:
+ *  Container for SPI configs for SPIClass. As above, libraries may reference it globally.
+ *
+ * These two classes are often provided by frameworks so we cannot extend them to add
+ * useful methods for Marlin.
+ *
+ * MarlinSPI:
+ *  Provides the default SPIClass interface plus some Marlin goodies such as a simplified
+ *  interface for SPI DMA transfer.
+ *
+ */
+
+using MarlinSPI = SPIClass;
